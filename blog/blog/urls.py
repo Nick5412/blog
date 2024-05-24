c60e73a5bc79c4d0cde8e231604de84572a72792
Nick <1374175412@qq.com>
2024-05-24 19:15:56 +0800

    配置redis与日志

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# 1.导入系统的logging
+import logging
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+# 2.获取日志器
+logger = logging.getLogger('django')
+
+
+def log(request):
+    # 3.使用日志记录器记录信息
+    logger.info('info')
+    return HttpResponse('test')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', log),
 ]
